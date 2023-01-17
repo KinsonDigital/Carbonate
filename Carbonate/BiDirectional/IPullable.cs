254c1467d0aa9921cecaaf9e7fc08ef7b07f85f2
Calvin Wilkinson <kinsondigital@gmail.com>
2023-01-17 19:10:26 +0000

    cleanup: remove code doc that is not required

--- Carbonate/BiDirectional/IPullable.cs
+++ Carbonate/BiDirectional/IPullable.cs
@@ -19,7 +19,6 @@ public interface IPullable<TDataIn, out TDataOut>
     /// </summary>
     /// <param name="data">The data to send to the responder.</param>
     /// <param name="respondId">The ID of the response.</param>
-    /// <typeparam name="TDataOut">The type of data going out.</typeparam>
     /// <returns>The data result going out.</returns>
     [SuppressMessage("ReSharper", "UnusedParameter.Global", Justification = "Public API.")]
     [SuppressMessage("ReSharper", "UnusedMemberInSuper.Global", Justification = "Public API.")]
