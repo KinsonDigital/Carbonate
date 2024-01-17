45826df693704694e6634fba8d8f30b1a7f963fc
Calvin Wilkinson <kinsondigital@gmail.com>
2024-01-17 14:36:27 +0000

    feat: create extension methods for one way respond subscriptions

--- Carbonate/ReactableExtensionMethods.cs
+++ Carbonate/ReactableExtensionMethods.cs
@@ -1,4 +1,4 @@
-// <copyright file="ReactableExtensionMethods.cs" company="KinsonDigital">
+﻿// <copyright file="ReactableExtensionMethods.cs" company="KinsonDigital">
 // Copyright (c) KinsonDigital. All rights reserved.
 // </copyright>
 
@@ -245,6 +245,125 @@ public static class ReactableExtensionMethods
             .WithId(id)
             .WithName(subName)
             .BuildOneWayReceive(onReceive);
+
+        return reactable.Subscribe(projOpenedSub);
+    }
+
+    /// <summary>
+    /// Creates a one-way directional reactable subscription for the given <paramref name="reactable"/>
+    /// with the given <paramref name="onRespond"/> that will be executed every time a notification is pushed from a source.
+    /// </summary>
+    /// <param name="reactable">The reactable to create a subscription with.</param>
+    /// <param name="id">The unique id.</param>
+    /// <param name="name">The name of the subscription.</param>
+    /// <param name="onRespond">The action to execute when receiving a notification.</param>
+    /// <typeparam name="TOut">The type of data coming from the subscription.</typeparam>
+    /// <returns>
+    ///     A <see cref="IDisposable"/> object that can be used to unsubscribe the subscription.
+    /// </returns>
+    /// <remarks>
+    ///     The data travels from the subscription and back to the source.
+    /// </remarks>
+    /// <exception cref="ArgumentException">
+    ///     Thrown if the following param are empty:
+    ///     <list type="bullet">
+    ///         <item><paramref name="id"/></item>
+    ///         <item><paramref name="name"/></item>
+    ///     </list>
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    ///     Thrown if the following are null:
+    ///     <list type="bullet">
+    ///         <item><paramref name="reactable"/></item>
+    ///         <item><paramref name="name"/></item>
+    ///         <item><paramref name="onRespond"/></item>
+    ///     </list>
+    /// </exception>
+    public static IDisposable CreateOneWayRespond<TOut>(
+        this IPullReactable<TOut> reactable,
+        Guid id,
+        string name,
+        Func<TOut> onRespond)
+    {
+        ArgumentNullException.ThrowIfNull(reactable);
+
+        if (id == Guid.Empty)
+        {
+            throw new ArgumentException("The id cannot be empty.", nameof(id));
+        }
+
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentNullException.ThrowIfNull(onRespond);
+
+        name = name.Trim();
+
+        var projOpenedSub = ISubscriptionBuilder.Create()
+            .WithId(id)
+            .WithName(name)
+            .BuildOneWayRespond(onRespond);
+
+        return reactable.Subscribe(projOpenedSub);
+    }
+
+    /// <summary>
+    /// Creates a one-way directional reactable subscription for the given <paramref name="reactable"/>
+    /// with the given <paramref name="onRespond"/> that will be executed every time a notification is pushed from a source.
+    /// </summary>
+    /// <param name="reactable">The reactable to create a subscription with.</param>
+    /// <param name="id">The unique id.</param>
+    /// <param name="onRespond">The action to execute when receiving a notification.</param>
+    /// <param name="callerMemberName">The name of the caller.</param>
+    /// <param name="callerFilePath">The file path of the caller.</param>
+    /// <typeparam name="TOut">The type of data coming from the subscription.</typeparam>
+    /// <returns>
+    ///     A <see cref="IDisposable"/> object that can be used to unsubscribe the subscription.
+    /// </returns>
+    /// <remarks>
+    ///     The data travels from the subscription and back to the source.
+    /// </remarks>
+    /// <exception cref="ArgumentException">
+    ///     Thrown if the <paramref name="id"/> param is empty.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    ///     Thrown if the following are null:
+    ///     <list type="bullet">
+    ///         <item><paramref name="reactable"/></item>
+    ///         <item><paramref name="onRespond"/></item>
+    ///     </list>
+    /// </exception>
+    public static IDisposable CreateOneWayRespond<TOut>(
+        this IPullReactable<TOut> reactable,
+        Guid id,
+        Func<TOut> onRespond,
+        [CallerMemberName] string callerMemberName = "",
+        [CallerFilePath] string callerFilePath = "")
+    {
+        ArgumentNullException.ThrowIfNull(reactable);
+
+        if (id == Guid.Empty)
+        {
+            throw new ArgumentException("The id cannot be empty.", nameof(id));
+        }
+
+        ArgumentNullException.ThrowIfNull(onRespond);
+        callerMemberName = string.IsNullOrEmpty(callerMemberName) ? string.Empty : callerMemberName;
+        callerFilePath = string.IsNullOrEmpty(callerFilePath) ? string.Empty : callerFilePath;
+
+        callerMemberName = callerMemberName.Trim();
+        callerFilePath = callerFilePath.Trim();
+
+        callerMemberName = callerMemberName.Contains('.')
+            ? callerMemberName.Split('.')[^1]
+            : callerMemberName;
+
+        var className = Path.GetFileNameWithoutExtension(callerFilePath);
+        var callerName = $"{className}.{callerMemberName}";
+        var subName = $"{callerName}() - {id}";
+
+        var projOpenedSub = ISubscriptionBuilder.Create()
+            .WithId(id)
+            .WithName(subName)
+            .BuildOneWayRespond(onRespond);
 
         return reactable.Subscribe(projOpenedSub);
     }
--- Testing/CarbonateTests/ReactableExtensionMethodsTests.cs
+++ Testing/CarbonateTests/ReactableExtensionMethodsTests.cs
@@ -223,6 +223,109 @@ public class ReactableExtensionMethodsTests
             .WithMessage("Value cannot be null. (Parameter 'onReceive')");
     }
     #endregion
+
+    #region CreateOneWayRespond With 4 Params
+    [Fact]
+    public void CreateOneWayRespond_WhenInvokingWithIdAndNameAndWithNullReactable_ThrowsException()
+    {
+        // Arrange
+        IPullReactable<int>? sut = null;
+
+        // Act
+        var act = () => sut.CreateOneWayRespond(Guid.Empty, "test-name", () => 10);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>()
+            .WithMessage("Value cannot be null. (Parameter 'reactable')");
+    }
+
+    [Fact]
+    public void CreateOneWayRespond_WhenInvokingWithIdAndNameAndWithEmptyId_ThrowsException()
+    {
+        // Arrange
+        var sut = new PullReactable<int>();
+
+        // Act
+        var act = () => sut.CreateOneWayRespond(Guid.Empty, "test-name", () => 20);
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("The id cannot be empty. (Parameter 'id')");
+    }
+
+    [Theory]
+    [InlineData(null, "Value cannot be null. (Parameter 'name')")]
+    [InlineData("", "The value cannot be an empty string. (Parameter 'name')")]
+    public void CreateOneWayRespond_WhenInvokingWithIdAndNameAndWithNullOrEmptyName_ThrowsException(string? name, string expected)
+    {
+        // Arrange
+        var sut = new PullReactable<int>();
+
+        // Act
+        var act = () => sut.CreateOneWayRespond(Guid.NewGuid(), name, null);
+
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage(expected);
+    }
+
+    [Fact]
+    public void CreateOneWayRespond_WhenInvokingWithIdAndNameAndWithNullOnReceive_ThrowsException()
+    {
+        // Arrange
+        var sut = new PullReactable<int>();
+
+        // Act
+        var act = () => sut.CreateOneWayRespond(Guid.NewGuid(), "test-name", null);
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("Value cannot be null. (Parameter 'onRespond')");
+    }
+    #endregion
+
+    #region CreateOneWayRespond With 5 Params
+    [Fact]
+    public void CreateOneWayRespond_WhenInvokingWithIdAndAutoNameAndWithNullReactable_ThrowsException()
+    {
+        // Arrange
+        IPullReactable<int>? sut = null;
+
+        // Act
+        var act = () => sut.CreateOneWayRespond(Guid.Empty, () => 30);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>()
+            .WithMessage("Value cannot be null. (Parameter 'reactable')");
+    }
+
+    [Fact]
+    public void CreateOneWayRespond_WhenInvokingWithIdAndAutoNameAndWithEmptyId_ThrowsException()
+    {
+        // Arrange
+        var sut = new PullReactable<int>();
+
+        // Act
+        var act = () => sut.CreateOneWayRespond(Guid.Empty, () => 40);
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("The id cannot be empty. (Parameter 'id')");
+    }
+
+    [Fact]
+    public void CreateOneWayRespond_WhenInvokingWithIdAndAutoNameAndWithNullOnRespond_ThrowsException()
+    {
+        // Arrange
+        var sut = new PullReactable<int>();
+
+        // Act
+        var act = () => sut.CreateOneWayRespond(Guid.NewGuid(), null);
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("Value cannot be null. (Parameter 'onRespond')");
+    }
+    #endregion
     #endregion
 
     #region Method Non-Exception Tests
@@ -294,6 +397,43 @@ public class ReactableExtensionMethodsTests
 
         // Act
         var unsubscriber = sut.CreateOneWayReceive(id, expectedAction);
+
+        // Assert
+        Assert.NotNull(unsubscriber);
+        sut.SubscriptionIds.Should().BeEquivalentTo(expectedIds);
+    }
+
+    [Fact]
+    [SuppressMessage("ReSharper", "ConvertToLocalFunction", Justification = "Not required for testing.")]
+    public void CreateOneWayRespond_WhenInvokingWithIdAndName_CreatesSubscription()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var expectedIds = new[] { id }.AsReadOnly();
+        const string expectedName = "test-name";
+        var expectedAction = () => 123;
+        var sut = new PullReactable<int>();
+
+        // Act
+        var unsubscriber = sut.CreateOneWayRespond(id, expectedName, expectedAction);
+
+        // Assert
+        Assert.NotNull(unsubscriber);
+        sut.SubscriptionIds.Should().BeEquivalentTo(expectedIds);
+    }
+
+    [Fact]
+    [SuppressMessage("ReSharper", "ConvertToLocalFunction", Justification = "Not required for testing.")]
+    public void CreateOneWayRespond_WhenInvokingWithIdAndAutoName_CreatesSubscription()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var expectedIds = new[] { id }.AsReadOnly();
+        var expectedAction = () => 456;
+        var sut = new PullReactable<int>();
+
+        // Act
+        var unsubscriber = sut.CreateOneWayRespond(id, expectedAction);
 
         // Assert
         Assert.NotNull(unsubscriber);
