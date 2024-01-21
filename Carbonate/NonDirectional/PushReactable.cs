bce1f4d54e777164b6fad63945f6c32e40e169fb
Calvin Wilkinson <kinsondigital@gmail.com>
2024-01-21 14:56:16 +0000

    chore: move error notification processing to base class

--- Carbonate/NonDirectional/PushReactable.cs
+++ Carbonate/NonDirectional/PushReactable.cs
@@ -35,38 +35,4 @@ public class PushReactable : ReactableBase<IReceiveSubscription>, IPushReactable
             SendError(e, id);
         }
     }
-
-    /// <summary>
-    /// Sends an error to all of the subscribers that match the given <paramref name="id"/>.
-    /// </summary>
-    /// <param name="exception">The exception that occurred.</param>
-    /// <param name="id">The ID of the event where the notification will be pushed.</param>
-    private void SendError(Exception exception, Guid id)
-    {
-        /* Work from the end to the beginning of the list
-         * just in case the reactable is disposed(removed)
-         * in the OnReceive() method.
-         */
-        for (var i = InternalSubscriptions.Count - 1; i >= 0; i--)
-        {
-            /*NOTE:
-             * The purpose of this logic is to prevent array index errors
-             * if an OnReceive() implementation ends up unsubscribing a single
-             * subscription or unsubscribing from a single event id
-             *
-             * If the current index is not less than or equal to
-             * the total number of items, reset the index to the last item
-             */
-            i = i > InternalSubscriptions.Count - 1
-                ? InternalSubscriptions.Count - 1
-                : i;
-
-            if (InternalSubscriptions[i].Id != id)
-            {
-                continue;
-            }
-
-            InternalSubscriptions[i].OnError(exception);
-        }
-    }
 }
